e142025e1ed13b753c9f36b7c35efc6ac0a109bc
Oleksiy Pokormyakho <116438996+OleksiyPok@users.noreply.github.com>
2025-05-04 19:02:07 +0200

    main.hpp has been added

---
+++ main.hpp
@@ -0,0 +1,14 @@
+
+// HW-04. Імплементація логера на патернах проектування
+//-----------------------------------------------
+
+#pragma once
+
+#include <algorithm>
+#include <string>
+
+#include "./hpp/logger.hpp"
+
+using string = std::string;
+
+int makeLogging(int argc, char **argv);
